33777580fdecf0b76c2fd5a15e7b4cd9644f4fd0
jeanzada01 <128435986+jeanzada01@users.noreply.github.com>
2024-09-15 11:38:40 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,4 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita o envio do formulário para fins de demonstração
+    alert('Formulário enviado!');
+});
